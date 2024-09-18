1c7de2825b41a3b1a14b37ebd30b37a37f48687f
Srijan Verma <verma.srijan19@gmail.com>
2024-09-18 23:11:46 +0530

    Mongo DB data access working now need to list out the APIs

--- server/src/main/kotlin/org/lifesparktech/lt_internal/Application.kt
+++ server/src/main/kotlin/org/lifesparktech/lt_internal/Application.kt
@@ -1,20 +1,21 @@
 package org.lifesparktech.lt_internal
 
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Projections
+import com.mongodb.client.model.Updates
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.resources.Resources
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 import io.ktor.server.response.*
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
 import kotlinx.coroutines.flow.toList
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import org.bson.codecs.pojo.annotations.BsonId
-import org.bson.types.ObjectId
-
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module).start(wait = true)
@@ -31,16 +32,41 @@ fun Application.module() {
     val mongoDatabase = connectToMongoDB()
     configureAuthentication()
     install(Resources)
+
+    fun getRazorpayEventsCollection() = mongoDatabase.getCollection<RazorpayEvent>("razorpayEvents")
+
     routing {
-        get("/getOrders") {
-            val collection= mongoDatabase.getCollection<RazorpayEvent>("razorpayEvents")
-            val events= collection
-            call.respond(events)
+        route("/orders") {
+            get("/") {
+                val collection = getRazorpayEventsCollection()
+                val filter = Filters.eq(RazorpayEvent::status.name, "captured")
+                val projection = Projections.fields(
+                    Projections.include(
+                        RazorpayEvent::id.name,
+                        RazorpayEvent::email.name,
+                        RazorpayEvent::contact.name,
+                        RazorpayEvent::status.name,
+                        RazorpayEvent::amount.name
+                    )
+                )
+                val events = collection.find(filter).projection(projection).toList()
+                call.respond(events)
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]
+                val collection = getRazorpayEventsCollection()
+                val filter = Filters.eq(RazorpayEvent::id.name, "$id")
+                val update = Updates.set(RazorpayEvent::status.name, "Cool")
+                collection.updateOne(filter, update)
+                call.respond("Updated")
+            }
         }
     }
 }
+
 @Serializable
 data class RazorpayEvent(
+    val id: String,
     val email: String,
     val contact: String,
     val status: String,
